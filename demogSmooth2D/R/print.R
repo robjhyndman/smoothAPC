afb5de6a5098596b3892ec6df099e67bda41c81f
robjhyndman <robjhyndman@gmail.com>
2016-09-14 16:07:46 +0200

    Removed print.sm2D method from documentation

--- demogSmooth2D/R/print.R
+++ demogSmooth2D/R/print.R
@@ -1,17 +1,18 @@
-#' Prints the result of smoothing (object of class \code{sm2D}).
-#'
-#' @param x Result of smoothing (object of class \code{sm2D}).
-#' @param ... Other parameters. They are currently ignored.
-#' @examples
-#' \dontrun{
-#'
-#' library(demography)
-#' m = log(fr.mort$rate$female[1:30, 150:160])
-#' sm = autoDemogSmooth(m)
-#' sm
-#'
-#' }
-#' @author Alexander Dokumentov
+# Prints the result of smoothing (object of class \code{sm2D}).
+#
+# @param x Result of smoothing (object of class \code{sm2D}).
+# @param ... Other parameters. They are currently ignored.
+# @examples
+# \dontrun{
+#
+# library(demography)
+# m = log(fr.mort$rate$female[1:30, 150:160])
+# sm = autoDemogSmooth(m)
+# sm
+#
+# }
+# @author Alexander Dokumentov
+
 #' @export
 
 print.sm2D = function(x, ...)
